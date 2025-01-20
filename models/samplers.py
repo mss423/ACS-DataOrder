1fc679234354301d1202f8b7b61843922a3e0414
Max Springer <mss423@cornell.edu>
2025-01-20 16:57:02 -0500

    Update samplers.py

--- models/samplers.py
+++ models/samplers.py
@@ -1,58 +1,92 @@
 import math
-
 import torch
 
+def squared_error(ys_pred, ys):
+    return (ys - ys_pred).square()
 
-class DataSampler:
-    def __init__(self, n_dims):
-        self.n_dims = n_dims
+def mean_squared_error(ys_pred, ys):
+    return (ys - ys_pred).square().mean()
 
-    def sample_xs(self):
-        raise NotImplementedError
+def accuracy(ys_pred, ys):
+    return (ys == ys_pred.sign()).float()
 
+sigmoid = torch.nn.Sigmoid()
+bce_loss = torch.nn.BCELoss()
 
-def get_data_sampler(data_name, n_dims, **kwargs):
-    names_to_classes = {
-        "gaussian": GaussianSampler,
-    }
-    if data_name in names_to_classes:
-        sampler_cls = names_to_classes[data_name]
-        return sampler_cls(n_dims, **kwargs)
+def get_gaussian_samples(n_dims, n_points, seed=None, scale=None, bias=None):
+    if seed is None:
+        xs = torch.randn(n_points, n_dims)
     else:
-        print("Unknown sampler")
+        generator = torch.Generator()
+        generator.manual_seed(seed)
+        xs = torch.randn(n_points, n_dims)
+
+    if self.scale is not None:
+        xs = xs @ scale
+    if self.bias is not None:
+        xs += bias
+    return xs
+
+def get_random_function(task, n_dims):
+    if task == "linear":
+
+    else:
+        rprint("Unknown task")
         raise NotImplementedError
 
+class Task:
+    def __init__(self, n_dims, pool_dict=None, seeds=None):
+        self.n_dims = n_dims
+        self.pool_dict = pool_dict
+        self.seeds = seeds
+        assert pool_dict is None or seeds is None
 
-def sample_transformation(eigenvalues, normalize=False):
-    n_dims = len(eigenvalues)
-    U, _, _ = torch.linalg.svd(torch.randn(n_dims, n_dims))
-    t = U @ torch.diag(eigenvalues) @ torch.transpose(U, 0, 1)
-    if normalize:
-        norm_subspace = torch.sum(eigenvalues**2)
-        t *= math.sqrt(n_dims / norm_subspace)
-    return t
+    def evaluate(self, xs):
+        raise NotImplementedError
 
+    @staticmethod
+    def generate_pool_dict(n_dims, num_tasks):
+        raise NotImplementedError
 
-class GaussianSampler(DataSampler):
-    def __init__(self, n_dims, bias=None, scale=None):
-        super().__init__(n_dims)
-        self.bias = bias
+    @staticmethod
+    def get_metric():
+        raise NotImplementedError
+
+    @staticmethod
+    def get_training_metric():
+        raise NotImplementedError
+
+class LinearRegression(Task):
+    def __init__(self, n_dims, pool_dict=None, seed=None, scale=1):
+        """scale: a constant by which to scale the randomly sampled weights."""
+        super(LinearRegression, self).__init__(n_dims, pool_dict, seeds)
         self.scale = scale
 
-    def sample_xs(self, n_points, b_size, n_dims_truncated=None, seeds=None):
-        if seeds is None:
-            xs_b = torch.randn(b_size, n_points, self.n_dims)
-        else:
-            xs_b = torch.zeros(b_size, n_points, self.n_dims)
+        if pool_dict is None and seeds is None:
+            self.w = torch.randn(self.n_dims, 1)
+        elif seed is not None:
+            self.w = torch.zeros(self.n_dims, 1)
             generator = torch.Generator()
-            assert len(seeds) == b_size
-            for i, seed in enumerate(seeds):
-                generator.manual_seed(seed)
-                xs_b[i] = torch.randn(n_points, self.n_dims, generator=generator)
-        if self.scale is not None:
-            xs_b = xs_b @ self.scale
-        if self.bias is not None:
-            xs_b += self.bias
-        if n_dims_truncated is not None:
-            xs_b[:, :, n_dims_truncated:] = 0
-        return xs_b
+            generator.manual_seed(seed)
+            self.w = torch.randn(self.n_dims, 1, generator=generator)
+        else:
+            assert "w" in pool_dict
+            indices = torch.randperm(len(pool_dict["w"]))[:batch_size]
+            self.w = pool_dict["w"][indices]
+
+    def evaluate(self, xs):
+        w = self.w.to(xs.device)
+        ys = self.scale * (xs @ w)[:, :, 0]
+        return ys
+
+    @staticmethod
+    def generate_pool_dict(n_dims, **kwargs):  # ignore extra args
+        return {"w": torch.randn(n_dims, 1)}
+
+    @staticmethod
+    def get_metric():
+        return squared_error
+
+    @staticmethod
+    def get_training_metric():
+        return mean_squared_error
