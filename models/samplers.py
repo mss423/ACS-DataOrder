3e6c0d0997b2f6a45c305e9f42e5bba45c3aefd4
Max Springer <mss423@cornell.edu>
2025-01-21 17:33:46 -0500

    Implemented DecisionTrees

--- models/samplers.py
+++ models/samplers.py
@@ -125,3 +125,71 @@ class SparseLinearRegression(LinearRegression):
     @staticmethod
     def get_training_metric():
         return mean_squared_error
+
+class DecisionTree(Task):
+    def __init__(self, n_dims, pool_dict=None, seeds=None, depth=4):
+
+        super(DecisionTree, self).__init__(n_dims, pool_dict, seeds)
+        self.depth = depth
+
+        if pool_dict is None:
+
+            # We represent the tree using an array (tensor). Root node is at index 0, its 2 children at index 1 and 2...
+            # dt_tensor stores the coordinate used at each node of the decision tree.
+            # Only indices corresponding to non-leaf nodes are relevant
+            self.dt_tensor = torch.randint(
+                low=0, high=n_dims, size=(1, 2 ** (depth + 1) - 1)
+            )
+
+            # Target value at the leaf nodes.
+            # Only indices corresponding to leaf nodes are relevant.
+            self.target_tensor = torch.randn(self.dt_tensor.shape)
+        elif seeds is not None:
+            self.dt_tensor = torch.zeros(1, 2 ** (depth + 1) - 1)
+            self.target_tensor = torch.zeros_like(dt_tensor)
+            generator = torch.Generator()
+            generator.manual_seed(seed)
+            self.dt_tensor[0] = torch.randint(
+                low=0,
+                high=n_dims - 1,
+                size=2 ** (depth + 1) - 1,
+                generator=generator,
+            )
+            self.target_tensor[0] = torch.randn(
+                self.dt_tensor[0].shape, generator=generator
+            )
+        else:
+            raise NotImplementedError
+
+    def evaluate(self, xs):
+        dt_tensor = self.dt_tensor.to(xs.device)
+        target_tensor = self.target_tensor.to(xs.device)
+
+        # Assume xs is a single data point
+        xs_bool = xs > 0
+
+        # Use the single decision tree
+        dt = dt_tensor[0]  
+        target = target_tensor[0] 
+
+        cur_nodes = torch.zeros(xs.shape[0], device=xs.device).long() 
+        for j in range(self.depth):
+            cur_coords = dt[cur_nodes]
+            cur_decisions = xs_bool[torch.arange(xs_bool.shape[0]), cur_coords]
+            cur_nodes = 2 * cur_nodes + 1 + cur_decisions
+
+        # Return the target value for the final node
+        return target[cur_nodes]
+
+    @staticmethod
+    def generate_pool_dict(n_dims, num_tasks, hidden_layer_size=4, **kwargs):
+        raise NotImplementedError
+
+    @staticmethod
+    def get_metric():
+        return squared_error
+
+    @staticmethod
+    def get_training_metric():
+        return mean_squared_error
+
