35f6a8a273b54af1698974d7f81256692606049d
Max Springer <mss423@cornell.edu>
2025-01-22 16:57:53 -0500

    Added distance graph computation

--- ordering/order_methods.py
+++ ordering/order_methods.py
@@ -58,11 +58,14 @@ def incremental_kmeans_ordering(data, Ks):
 
     return ordered_indices
 
-def max_cover_random(data, sim_thresh=0.7, seed=42):
+def max_cover_random(data, sim_thresh=0.7, seed=42, max_degree=None):
     # Runs max cover on graph with similarity threshold, then randomly permutes remaining data
     np.random.seed(seed)
-    cos_sim = cosine_similarity(data)
-    G = build_graph(cos_sim, sim_thresh=sim_thresh)
+
+    # cos_sim = cosine_similarity(data)
+    # G = build_graph(cos_sim, sim_thresh=sim_thresh, max_degree=max_degree)
+    
+    G = build_dist_graoh(data)
     samples, _ = max_cover(G, len(data))
 
     all_idx = set(list(range(len(data))))
@@ -70,38 +73,22 @@ def max_cover_random(data, sim_thresh=0.7, seed=42):
     # Return the max cover and randomly permuted remainder
     return samples, remaining_indices
 
-def max_cover_pseudo(data, sim_thresh=0.7, seed=42):
+def max_cover_pseudo(data, sim_thresh=0.7, seed=42, max_degree=None):
     # Runs max cover on graph with similarity threshold, then randomly permutes remaining data
     np.random.seed(seed)
-    cos_sim = cosine_similarity(data)
-    G = build_graph(cos_sim, sim_thresh=sim_thresh)
+
+    # cos_sim = cosine_similarity(data)
+    # G = build_graph(cos_sim, sim_thresh=sim_thresh, max_degree=max_degree)
+    
+    G = build_dist_graoh(data)
     samples = max_cover_sampling(G, len(data))
     return samples
-
-def max_k_cover(data, Ks, sim_thresh=0.7):
-    # Return K cover for a given similarity threshold
-    # Note: yields <= K samples
-    cos_sim = cosine_similarity(data)
-    selected_samples = {}
-    for K in Ks:
-        G = build_graph(cos_sim, sim_thresh=sim_thresh)
-        selected_samples[K], _ = max_cover(G, K)
-    return selected_samples
 
 def acs_k_cover(data, K):
     # For fixed K coverage, compute optimal threshold and return K samples
     cos_sim = cosine_similarity(data)
     _, _, samples = calculate_similarity_threshold(cos_sim, K, coverage=0.9, sims=[0,1000])
     return samples
-
-def acs_ks_range(data, Ks):
-    # Iterate over each K, run ACS to get samples
-    # Note: this ordering is NOT nested
-    cos_sim = cosine_similarity(data)
-    selected_samples = {}
-    for K in Ks:
-        _, _, selected_samples[K] = calculate_similarity_threshold(cos_sim, K, coverage=0.9)
-    return selected_samples
 
 def hierarchical_acs(data, covered=None):
     if covered and len(covered) == 1:
